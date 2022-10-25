8fd4c5540f6554afbbcbab0c77ac0f2a3bd80298
Horhe <imc950@hotmail.com>
2022-10-25 13:14:38 +0300

    nov update

--- public/scripts.js
+++ public/scripts.js
@@ -10,7 +10,6 @@ function setCookie() {
         const expires = "expires=" + date.toUTCString();
         document.cookie = doc.id + "=0,0,0; " + expires + "; path=/";
         var country = doc.id;
-        console.log(country)
         if (!(Object.keys(countryArr).includes(country))){
             countryArr[country] = "0,0,0";
             //countryArr.push(doc.id: "0,0,0");
@@ -19,9 +18,27 @@ function setCookie() {
     console.log(countryArr)
 }
 
-function rate() {
-    var show = document.getElementById("show")
-    var sing = document.getElementById("sing")
-    var song = document.getElementById("song")
+function setCookie(cName, cValue, expDays) {
+    let date = new Date();
+        date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = doc.id + "=0,0,0; " + expires + "; path=/";
+        var country = doc.id;
+        if (!(Object.keys(countryArr).includes(country))){
+            countryArr[country] = "0,0,0";
+            //countryArr.push(doc.id: "0,0,0");
+        }
+}
 
+function rate() {
+    var show = document.getElementById("show").value.toString();
+    var sing = document.getElementById("sing").value.toString();
+    var song = document.getElementById("song").value.toString();
+
+    var points = show + "," + sing + "," + song;
+    var toCook = "=" + points
+    console.log(points)
+    countryArr.den = points;
+    console.log(countryArr)
+    document.cookie = den + toCook 
 }
